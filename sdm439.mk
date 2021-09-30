27731489c8b1f9874f4eb50a77f295df651a0ae3
Chuck439 <firmankediri123@gmail.com>
2021-09-30 02:13:12 +0000

    sdm439-common: Drop XiaomiParts
    This reverts commit e8575ad4ef2952ad24bc5c7dfdf2b9e979f62e0e.
    
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- sdm439.mk
+++ sdm439.mk
@@ -60,10 +60,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
 
-# Device-specific Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
